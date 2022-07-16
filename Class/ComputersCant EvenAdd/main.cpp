85520045859bfb4a4e6cd3d7ba838a8d0535e45e
Manuel-M2904164 <109438061+Manuel-M2904164@users.noreply.github.com>
2022-07-16 13:41:01 -0700

    Add files via upload

---
+++ Class/ComputersCant EvenAdd/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * File:   main.cpp
+ * Author: Manuel Medina
+ *
+ * Created on June 21, 2022, 12:01 PM
+ * Purpose:  error illustrated when adding
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    short op1,op2,result;
+    
+    //initialize variables
+    op1=2;
+    op2=3;
+    
+    //Map inputs to outputs -> The Process
+    result=op1+op2;
+    
+    //Display Results
+    cout<<result<< " = "<<op1<<" + "<<op2<<endl;
+
+    //Exit Stage Right
+    return 0;
+}
+
---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * File:   main.cpp
+ * Author: Manuel Medina
+ *
+ * Created on June 21, 2022, 12:01 PM
+ * Purpose: Our first Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    cout<<"Hello World"<<endl;
+    //Another way to do the same thing!
+    cout<<"Hello "<<"World"<<endl;
+    cout
+            <<"Hello "
+            <<"World"
+            <<endl;
+    cout<<"Hello ";
+    cout<<"World";
+    cout<<endl;
+
+    //Exit Stage Right
+    return 0;
+}
+
